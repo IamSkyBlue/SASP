e4a32b8b64d4ab618bbcb0046b82e46805e7dbad
SkyBlue <bob890823@gmail.com>
2023-10-03 21:28:21 +0800

    feat: update package,readme,remove heroku file

--- README.md
+++ README.md
@@ -2,17 +2,23 @@
 
 SASP : SASP auto saves photos<br>
 Line 丟在聊天室的照片過七天就會過期不能存取，然後丟相簿又很麻煩，所以搞了個自動備份到 google drive 的機器人<br>
-<br>
-機器人本體在 heroku 上運行，透過 webhook 溝通<br>
-<br>
-app.py:主體，記得把 CHANNEL_SECRET 和 Line_bot_token 改成自己的<br>
-client_secrets.json:去自己 google api 頁面下載<br>
-Procfile:heroku config<br>
-requirements.txt:heroku config<br>
-runtime.txt:heroku config<br>
-settings.yaml: PyDrive 文件 https://pythonhosted.org/PyDrive/<br>
-verified_groups.txt:heroku 會自動休眠、重啟<br>
-所以就把 line_group_id 和 gdrive 資料夾 id 直接存起來這樣就不用每次重啟都找半天<br>
-<br>
-部屬前請先在自己電腦 run 一次，他會直接開瀏覽器做 authentication，資訊會直接存到 credentials.json<br>
-(我就偷懶 👍
+
+Steps:
+1. 創建Line bot: https://developers.line.biz/console ,並填寫機器接收webhook的url (https://myhost.net/callback),並開放Line bot接受群組邀請
+
+2. 把app.py中的 CHANNEL_SECRET 和 access_token 改成自己line bot的
+
+3. client_secrets.json:去自己 google api 頁面下載:
+GCP導覽列->
+API和服務->
+憑證->
+建立憑證->
+OAuth用戶端ID->
+應用程式類型選電腦應用程式->
+建立->
+下載json->
+放到資料夾中並改名為client_secrets.json
+
+4. 第一次啟動會開啟瀏覽器取得雲端硬碟權限,並將驗證資訊存至credentials.json檔案中,同時印出雲端硬碟資料夾ID,請填入至verified_groups.txt
+
+5. 邀請Line bot至群組,輸入!getgid,並將此group id填入至verified_groups.txt
--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 import random
 from flask import Flask, request, abort
+    
 
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    MessagingApiBlob,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent,
+    ImageMessageContent,
+    VideoMessageContent
+)
 
 import tempfile
 import os
 import time
 from datetime import datetime
 
-from pydrive.drive import GoogleDrive
-from pydrive.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+from pydrive2.auth import GoogleAuth
+
 
 gauth = GoogleAuth()
 
@@ -19,8 +38,8 @@ drive = GoogleDrive(gauth)
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi("YOUR_TOKEN_HERE")
-handler = WebhookHandler("YOUR_CHANNEL_SECRET")
+configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
 # get verified groups' id
@@ -29,10 +48,10 @@ with open("verified_groups.txt") as f:
     verified_groups_id = f.read().splitlines()
 
 
-@app.route("/callback", methods=["POST"])
+@app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
@@ -42,69 +61,49 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print(
-            "Invalid signature. Please check your channel access token/channel secret."
-        )
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
-    return "OK"
+    return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_TextMessage(event):
-    if event.message.text == "!getgid":
-        msg = TextMessage(text=event.source.group_id)
-        line_bot_api.reply_message(event.reply_token, msg)
+    with ApiClient(configuration) as api_client:
+        if event.message.text == "!getgid":
+            line_bot_api = MessagingApi(api_client)
+            msg = TextMessage(text=event.source.group_id)
+            line_bot_api.reply_message(event.reply_token, msg)
 
+def handle_blob(event,ext):
+    with ApiClient(configuration) as api_client:
+        try:
+            line_bot_api = MessagingApiBlob(api_client)
+            time.sleep(10)
+            dir_id = verified_groups_id[verified_groups_id.index(event.source.group_id) + 1]
+            message_content = line_bot_api.get_message_content(event.message.id)
+            with tempfile.NamedTemporaryFile(delete=False) as tf:
+                tf.write(message_content)    
+                tempfile_path = tf.name
+            dist_path = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "." + ext
+            os.rename(tempfile_path, dist_path)
+            file1 = drive.CreateFile({"parents": [{"id": dir_id}]})
+            file1.SetContentFile(dist_path)
+            file1.Upload()
+            return 0
+        except ValueError:
+            print("id: " + event.source.group_id + "not matching.")
+        except Exception as e:
+            print(e)
 
-@handler.add(MessageEvent, message=ImageMessage)
+@handler.add(MessageEvent, message=ImageMessageContent)
 def handle_ImageMessage(event):
-    try:
-        time.sleep(10)
-        dir_id = verified_groups_id[verified_groups_id.index(event.source.group_id) + 1]
-        ext = "jpg"
-        message_content = line_bot_api.get_message_content(event.message.id)
-        with tempfile.NamedTemporaryFile(
-            dir=os.path.dirname(__file__), delete=False
-        ) as tf:
-            for chunk in message_content.iter_content():
-                tf.write(chunk)
-            tempfile_path = tf.name
-        dist_path = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "." + ext
-        os.rename(tempfile_path, dist_path)
-        file1 = drive.CreateFile({"parents": [{"id": dir_id}]})
-        file1.SetContentFile(dist_path)
-        file1.Upload()
-        return 0
-    except ValueError:
-        print("no id found.")
-    except Exception as e:
-        print(e)
+    handle_blob(event,"jpg")
 
 
-@handler.add(MessageEvent, message=VideoMessage)
+@handler.add(MessageEvent, message=VideoMessageContent)
 def handle_ImageMessage(event):
-    try:
-        time.sleep(10)
-        dir_id = verified_groups_id[verified_groups_id.index(event.source.group_id) + 1]
-        ext = "mp4"
-        message_content = line_bot_api.get_message_content(event.message.id)
-        with tempfile.NamedTemporaryFile(
-            dir=os.path.dirname(__file__), delete=False
-        ) as tf:
-            for chunk in message_content.iter_content():
-                tf.write(chunk)
-            tempfile_path = tf.name
-        dist_path = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "." + ext
-        os.rename(tempfile_path, dist_path)
-        file1 = drive.CreateFile({"parents": [{"id": dir_id}]})
-        file1.SetContentFile(dist_path)
-        file1.Upload()
-        return 0
-    except ValueError:
-        print("no id found.")
-    except Exception as e:
-        print(e)
+    handle_blob(event,"mp4")
 
 
 if __name__ == "__main__":
